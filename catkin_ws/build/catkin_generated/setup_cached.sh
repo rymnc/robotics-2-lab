a2b2bf7a3adb4674083ffbb960ef772803860a9c
rymnc <aaryamannchallani7@gmail.com>
2020-12-21 08:35:29 +0000

    feat: initialized catkin_ws

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/aarya/Desktop/robolab/submission/robotics-2-lab/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/aarya/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/aarya/.local/bin/'
+export PWD='/home/aarya/Desktop/robolab/submission/robotics-2-lab/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/aarya/Desktop/robolab/submission/robotics-2-lab/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/aarya/Desktop/robolab/submission/robotics-2-lab/catkin_ws/src:$ROS_PACKAGE_PATH"
